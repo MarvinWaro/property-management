57f36a6ed0655ca1ad77d7594688b49d2b7160fa
MarvinWaro <mrvnwr.dev@gmail.com>
2025-06-10 11:25:41 +0800

    test 2

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -149,7 +149,7 @@
                 @if (auth()->user()->role === 'admin' && !$isAssetsMode)
                     <!-- Supplies Mode Tabs -->
                     <a href="{{ route('dashboard') }}"
-                       class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm transition-all duration-200 {{ request()->routeIs('dashboard') ? '!border-[#ce201f] text-[#ce201f] dark:border-[#ce201f] dark:text-[#ce201f]' : '' }}">
+                       class=" text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm transition-all duration-200 {{ request()->routeIs('dashboard') ? 'border-[#ce201f] text-[#ce201f] dark:border-[#ce201f] dark:text-[#ce201f]' : 'border-transparent' }}">
                         <span class="flex items-center space-x-2">
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
