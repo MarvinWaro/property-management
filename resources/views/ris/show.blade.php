d4c5ac2f75b88016c3406902c57f9273fbdbeb7c
MarvinWaro <mrvnwr.dev@gmail.com>
2025-06-24 13:50:50 +0800

    done on the decline part

--- resources/views/ris/show.blade.php
+++ resources/views/ris/show.blade.php
@@ -35,7 +35,7 @@
                             Print RIS
                         </a>
 
-                        <!-- Excel Export Button -->
+                        <!-- Excel Export Buttons -->
                         <a href="{{ route('ris.export-excel', $risSlip) }}"
                         class="inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
