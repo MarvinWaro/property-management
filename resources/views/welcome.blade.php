66a4dcfc54d0eec82367cc36c0c42f0421522fa7
MarvinWaro <mrvnwr.dev@gmail.com>
2025-07-09 00:35:39 +0800

    Update favicon and page titles in layouts and welcome page
    Replaced the apple-touch-icon and favicon links in app and guest layouts to use updated image paths and added favicon.ico. Updated the welcome page title to reflect 'CHED Inventory and Supply Management System' instead of 'Comprehensive Inventory and Supply Management System'.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>CISMS XII - Comprehensive Inventory and Supply Management System</title>
+        <title>CISMS XII - CHED Inventory and Supply Management System</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
