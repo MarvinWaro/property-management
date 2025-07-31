900a75766e51bef20c8b3d5f0d3d43a56bb1f8bc
MarvinWaro <mrvnwr.dev@gmail.com>
2025-07-31 12:00:26 +0800

    UPDATED READY FOR TESTING AND REMOVED PDF

--- resources/views/ris/show.blade.php
+++ resources/views/ris/show.blade.php
@@ -34,10 +34,10 @@
                     </div>
 
                     <div class="flex items-center space-x-2">
-                        <a href="{{ route('ris.print', $risSlip) }}" target="_blank"
+                        {{-- <a href="{{ route('ris.print', $risSlip) }}" target="_blank"
                             class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                             Print RIS
-                        </a>
+                        </a> --}}
 
                         <!-- Excel Export Buttons -->
                         <a href="{{ route('ris.export-excel', $risSlip) }}"
